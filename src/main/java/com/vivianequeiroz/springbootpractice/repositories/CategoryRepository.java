b2ff51538cb5bddfae671937105b3853bd934a16
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-10 12:03:46 -0300

    chore: category repository to manipulate and store data in memory from an entity

---
+++ src/main/java/com/vivianequeiroz/springbootpractice/repositories/CategoryRepository.java
@@ -0,0 +1,30 @@
+package com.vivianequeiroz.springbootpractice.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.vivianequeiroz.springbootpractice.entities.Category;
+
+// a class which objective is access and manipulate data from a specific entity
+
+@Component
+public class CategoryRepository {
+	//a collection with a pair of key and value. It is easier to locale an obj through id
+	private Map<Long, Category> map = new HashMap<>();
+
+	public void save(Category obj) {
+		map.put(obj.getId(), obj);
+	}
+
+	public Category findCategoryById(Long id) {
+		return map.get(id);
+	}
+	
+	public List<Category> findAllCategories() {
+		return new ArrayList<Category>(map.values());
+	}
+}
