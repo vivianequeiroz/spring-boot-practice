2471d5af90f6e4fa2c0971625266dc1a8181e98d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-10 11:20:40 -0300

    chore: initial project structure with spring boot

---
+++ src/test/java/com/vivianequeiroz/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivianequeiroz.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
