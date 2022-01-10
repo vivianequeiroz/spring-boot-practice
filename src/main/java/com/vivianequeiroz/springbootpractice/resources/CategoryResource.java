6270ee97c8c437130119b528eac40468af4865a4
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-10 11:54:48 -0300

    chore: boilerplate of @PathVariable notation usage to return a single obj

--- src/main/java/com/vivianequeiroz/springbootpractice/resources/CategoryResource.java
+++ src/main/java/com/vivianequeiroz/springbootpractice/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +14,25 @@ import com.vivianequeiroz.springbootpractice.entities.Category;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
-	// the method that always will be executed when a get request is made:
+
+	// the method that will always be executed when a get request is made:
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAllCategories() {
 		List<Category> list = new ArrayList<>();
-		
+
 		list.add(new Category(1L, "Eletronics"));
 		list.add(new Category(2L, "Books"));
-		
+
 		return ResponseEntity.ok().body(list);
 	}
 
-	/*
-	 * 
-	 * @GetMapping(value = "/{id}") public ResponseEntity<...>
-	 * findById(@PathVariable Long id) { ... return ResponseEntity.ok().body(...); }
-	 */
+	// @PathVariable Long id can be used when requesting a single object
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findCategoryById(@PathVariable Long id) {
+		Category categoryFound = (new Category(1L, "Eletronics"));
+		return ResponseEntity.ok().body(categoryFound);
+	}
+
 }
